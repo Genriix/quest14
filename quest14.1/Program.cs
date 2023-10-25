37bbd5abc32950fb56e5978ee9eb8625361348e7
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2023-10-25 11:54:14 +0300

    Добавьте файлы проекта.

---
+++ quest14.1/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quest14._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> russianToEnglishDictionary = new Dictionary<string, string>()
+            {
+                {"солнце", "sun"},
+                {"дождь", "rain"},
+                {"ветер", "wind"},
+                {"снег", "snow"},
+                {"туман", "fog"},
+                {"гроза", "thunderstorm"},
+                {"облачно", "cloudy"},
+                {"тепло", "warm"},
+                {"холодно", "cold"},
+                {"влажность", "humidity"}
+            };
+
+            Dictionary<string, string> russianToUkrainianDictionary = new Dictionary<string, string>()
+            {
+                {"сонце", "сонце"},
+                {"дощ", "дощ"},
+                {"вітер", "вітер"},
+                {"сніг", "сніг"},
+                {"туман", "туман"},
+                {"гроза", "гроза"},
+                {"хмарно", "хмарно"},
+                {"тепло", "тепло"},
+                {"холодно", "холодно"},
+                {"вологість", "вологість"}
+            };
+
+            Console.WriteLine("Введите слово на русском языке:");
+            string input = Console.ReadLine();
+
+            string translation = Translate(input, russianToEnglishDictionary);
+            if (translation != null)
+            {
+                Console.WriteLine($"Английский перевод: {translation}");
+            }
+            else
+            {
+                translation = Translate(input, russianToUkrainianDictionary);
+                if (translation != null)
+                {
+                    Console.WriteLine($"Украинский перевод: {translation}");
+                }
+                else
+                {
+                    Console.WriteLine("Такого слова нет в словаре.");
+                }
+            }
+
+            Console.ReadLine();
+        }
+
+        static string Translate(string word, Dictionary<string, string> dictionary)
+        {
+            if (dictionary.ContainsKey(word))
+            {
+                return dictionary[word];
+            }
+            else
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ quest14.2/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quest14._2
+{
+    class Program
+    {
+        static void Calculate(int n)
+        { 
+            int[] array = new int[n];
+            Console.WriteLine("Введите элементы массива:");
+
+            for (int i = 0; i < n; i++) 
+            { 
+                array[i] = Convert.ToInt32(Console.ReadLine()); 
+            }
+
+            int max = array[0];
+            int min = array[0];
+            int sum = 0;
+            double average;
+            Console.Write("Нечетные значения массива: ");
+            foreach (int num in array)
+            {
+                if (num > max) { max = num; }
+                if (num < min) { min = num; }
+                sum += num;
+                if (num % 2 != 0) { Console.Write(num + " "); }
+            }
+            average = (double)sum / n;
+            Console.WriteLine($"\n" +
+                $"Наибольшее значение массива: {max}\n" +
+                $"Наименьшее значение массива: {min}\n" +
+                $"Общая сумма элементов массива: {sum}\n" +
+                $"Среднее арифметическое всех элементов массива: {average}\n" +
+                $"\n" +
+                $"Для выхода введите q, для продолжения нажмите enter\n");
+            Console.ReadKey();
+            Console.Clear();
+        }
+        static void Main()
+        {
+            string s = "";
+            while (s != "q") 
+            {
+                Console.WriteLine("" +
+                    "Для выхода введите q\n" +
+                    "Введите размерность массива:");
+                s = Console.ReadLine();
+                try 
+                {
+                    Calculate(int.Parse(s)); // Метод выполнения задания
+                } 
+                catch 
+                {
+                    Console.WriteLine("" +
+                        "\nОшибка ввода\n" +
+                        "Введите числовые значения!!!\n");
+                }
+            }
+        }
+    }
+}
