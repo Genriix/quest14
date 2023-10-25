818fa961b89c206934828f6278f88e17e361d4bc
Vladislav Il'in <vladi5lav.ilin@yandex.ru>
2023-10-25 12:09:09 +0300

    v1.5

--- quest14.3/Program.cs
+++ quest14.3/Program.cs
@@ -6,10 +6,88 @@ using System.Threading.Tasks;
 
 namespace quest14._3
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Введите количество строк матрицы:");
+            int rows = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите количество столбцов матрицы:");
+            int cols = Convert.ToInt32(Console.ReadLine());
+
+            MyMatrix matrix = new MyMatrix(rows, cols);
+
+            matrix.FillMatrix();
+
+            Console.WriteLine("Исходная матрица:");
+            matrix.PrintMatrix();
+
+            Console.WriteLine("Производные матрицы:");
+            matrix.PrintDerivedMatrices();
+
+            Console.ReadLine();
+        }
+    }
+
+    class MyMatrix
+    {
+        private int[,] matrix;
+
+        public MyMatrix(int rows, int cols)
+        {
+            matrix = new int[rows, cols];
+        }
+
+        public void FillMatrix()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = random.Next(1, 10);
+                }
+            }
+        }
+
+        public void PrintMatrix()
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        public void PrintDerivedMatrices()
+        {
+            for (int i = matrix.GetLength(0); i > 0; i--)
+            {
+                int[,] derivedMatrix = new int[i, i];
+
+                for (int j = 0; j < derivedMatrix.GetLength(0); j++)
+                {
+                    for (int k = 0; k < derivedMatrix.GetLength(1); k++)
+                    {
+                        derivedMatrix[j, k] = matrix[j, k];
+                    }
+                }
+
+                Console.WriteLine($"Матрица порядка {i}:");
+                for (int j = 0; j < derivedMatrix.GetLength(0); j++)
+                {
+                    for (int k = 0; k < derivedMatrix.GetLength(1); k++)
+                    {
+                        Console.Write(derivedMatrix[j, k] + " ");
+                    }
+                    Console.WriteLine();
+                }
+            }
         }
     }
 }
--- quest14.4/Program.cs
+++ quest14.4/Program.cs
@@ -6,10 +6,117 @@ using System.Threading.Tasks;
 
 namespace quest14._4
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Store store = new Store();
+
+            store.AddArticle(new Article("Товар 1", "Магазин 1", 100));
+            store.AddArticle(new Article("Товар 2", "Магазин 2", 200));
+            store.AddArticle(new Article("Товар 3", "Магазин 3", 300));
+
+            Console.WriteLine("Введите номер товара:");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            Article articleByNumber = store.GetArticleByNumber(number);
+            if (articleByNumber != null)
+            {
+                Console.WriteLine("Название товара: " + articleByNumber.Title);
+                Console.WriteLine("Название магазина: " + articleByNumber.StoreName);
+                Console.WriteLine("Стоимость товара: " + articleByNumber.Price + " грн");
+            }
+            else
+            {
+                Console.WriteLine("Товар с указанным номером не найден");
+            }
+
+            Console.WriteLine("Введите название товара:");
+            string title = Console.ReadLine();
+
+            Article articleByName = store.GetArticleByName(title);
+            if (articleByName != null)
+            {
+                Console.WriteLine("Название товара: " + articleByName.Title);
+                Console.WriteLine("Название магазина: " + articleByName.StoreName);
+                Console.WriteLine("Стоимость товара: " + articleByName.Price + " грн");
+            }
+            else
+            {
+                Console.WriteLine("Товар с указанным названием не найден");
+            }
+
+            Console.ReadLine();
+        }
+    }
+
+    class Article
+    {
+        private string title;
+        private string storeName;
+        private double price;
+
+        public string Title
+        {
+            get { return title; }
+        }
+
+        public string StoreName
+        {
+            get { return storeName; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+        }
+
+        public Article(string title, string storeName, double price)
+        {
+            this.title = title;
+            this.storeName = storeName;
+            this.price = price;
+        }
+    }
+
+    class Store
+    {
+        private Article[] articles;
+
+        public Store()
+        {
+            articles = new Article[0];
+        }
+
+        public void AddArticle(Article article)
+        {
+            Array.Resize(ref articles, articles.Length + 1);
+            articles[articles.Length - 1] = article;
+        }
+
+        public Article GetArticleByNumber(int number)
+        {
+            if (number >= 0 && number < articles.Length)
+            {
+                return articles[number];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public Article GetArticleByName(string name)
+        {
+            foreach (Article article in articles)
+            {
+                if (article.Title == name)
+                {
+                    return article;
+                }
+            }
+
+            return null;
         }
     }
 }
